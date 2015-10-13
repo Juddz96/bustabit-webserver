bb18f16fc105098091dda2fdd6847b7bf3f21065
Ryan Havar <ryan@moneypot.com>
2015-10-13 13:25:25 +0000

    Fix autobet

--- client_old/scripts/strategies/auto-bet/logic.js
+++ client_old/scripts/strategies/auto-bet/logic.js
@@ -47,7 +47,7 @@ define(['lib/clib'], function(Clib){
 
                 var fixedCurrentBet = Math.round(currentBet / 100) * 100;
 
-                if(fixedCurrentBet > 0 && fixedCurrentBet <= engine.getBalance() && fixedCurrentBet <= engine.getMaxBet() && fixedCurrentBet <= maxBetStop) {
+                if(fixedCurrentBet > 0 && fixedCurrentBet <= engine.getBalance() && fixedCurrentBet <= engine.getMaxBet() && fixedCurrentBet < maxBetStop) {
                     engine.placeBet(fixedCurrentBet, Math.round(autoCashAt * 100), false);
                 } else {
                     engine.stop();
